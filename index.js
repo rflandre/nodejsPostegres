f248690b3160b0e55ad9ef40369767ff59209586
Renaud Flandre <rflandre@rflandre-ltm1.internal.salesforce.com>
2019-11-24 23:43:54 +0100

    Add listen 5

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var pg = require ('pg');
 
 const pool = new pg.Pool({
   connectionString: 'postgres://ilolhcujlhedox:2be8ff85d49d5b1022b4ff7b3c55a81f8c636d9b604ca8fc3df89bafcfa652ff@ec2-54-217-206-65.eu-west-1.compute.amazonaws.com:5432/d52bf94gqsc648',
+  ssl: true
 });
 
 var EventEmitter = require('events');
