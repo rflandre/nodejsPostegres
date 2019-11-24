5802aaaba4bf161ffb28513f9190c2443868b4ec
Renaud Flandre <rflandre@rflandre-ltm1.internal.salesforce.com>
2019-11-24 19:24:39 +0100

    Add listen

--- index.js
+++ index.js
@@ -8,3 +8,38 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+var pg = require ('pg');
+var EventEmitter = require('events');
+var util = require('util');
+
+// Build and instantiate our custom event emitter
+function DbEventEmitter(){
+  EventEmitter.call(this);
+}
+
+util.inherits(DbEventEmitter, EventEmitter);
+var dbEventEmitter = new DbEventEmitter;
+
+// Define the event handlers for each channel name
+dbEventEmitter.on('new_order', (msg) => {
+  // Custom logic for reacting to the event e.g. firing a webhook, writing a log entry etc
+  console.log('New order received: ' + msg.orderId);
+});
+
+// Connect to Postgres (replace with your own connection string)
+pg.connect('postgres://postgres:password@localhost:5432/postgres', function(err, client) {
+  if(err) {
+    console.log(err);
+  }
+
+  // Listen for all pg_notify channel messages
+  client.on('notification', function(msg) {
+    let payload = JSON.parse(msg.payload);
+    dbEventEmitter.emit(msg.channel, payload);
+  });
+  
+  // Designate which channels we are listening on. Add additional channels with multiple lines.
+  client.query('LISTEN new_order');
+});
